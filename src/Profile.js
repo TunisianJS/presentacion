e5dfbba312d7c4100b136ea62209f223ebab6aaa
moumniheithem <moumniheithem@gmail.com>
2021-05-02 04:04:41 +0100

    style: fix displaying items

--- src/Profile.js
+++ src/Profile.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import P from './Paragraph';
 
-const getFilter = isNotPresent => {
+const getFilter = (isNotPresent) => {
   if (isNotPresent) {
     return 'grayscale(100%)';
   }
@@ -14,14 +14,11 @@ const Profile = ({ isNotPresent, name, avatar, github }) => {
   return (
     <div
       style={{
-        display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'center',
-        padding: '2rem',
-        width: '25%'
+        flexWwrap: 'wrap',
+        padding: '1.5rem',
       }}
     >
-      {/*TODO: add flex */}
       <a href={github} style={{ textDecoration: 'none' }}>
         <img
           src={avatar}
@@ -29,8 +26,8 @@ const Profile = ({ isNotPresent, name, avatar, github }) => {
           style={{
             filter: getFilter(isNotPresent),
             margin: '0 auto',
-            height: '300px',
-            width: '300px'
+            height: '160px',
+            width: '160px',
           }}
         />
 
@@ -44,11 +41,11 @@ Profile.propTypes = {
   isNotPresent: PropTypes.bool,
   name: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
-  github: PropTypes.string.isRequired
+  github: PropTypes.string.isRequired,
 };
 
 Profile.defaultProps = {
-  isNotPresent: false
+  isNotPresent: false,
 };
 
 export default Profile;
